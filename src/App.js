3c16c3b1a30ab8b30678c53ffdf52fb4ba8dd804
Baljinder Singh <baljinder9219@gmail.com>
2021-12-13 16:28:14 +0530

    lets see

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import CitySearch from "./Components/CitySearch";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cp2 from "./Components/cp2";
-import Cp1 from "./Components/cp1";
-import Navbar from "./Components/navbar.jsx/Navbar";
-import "tachyons";
+
 import Weather from "./Components/wheather/weather";
 function App(){
 
@@ -19,7 +16,6 @@ function App(){
   return (
     <>
       <Router>
-        <h1>ji</h1>
         <Routes>
           <Route path='/' element={<CitySearch mfunct={mainfunction} />} />
         <Route path="/weather" element={<Weather cityid={cityid}/>}/>
